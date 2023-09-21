4eb2892d80bb6ec2a5a6317d5df76caf599202a8
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-09-20 21:51:32 -0300

    feat: implements signup and signin and return access_token in login

---
+++ api/src/modules/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { env } from 'src/shared/config/env';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [
+    JwtModule.register({
+      global: true,
+      signOptions: { expiresIn: '7d' },
+      secret: env.jwtSecret,
+    }),
+  ],
+})
+export class AuthModule {}
