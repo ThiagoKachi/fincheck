6fda4bf83de85e7ecfbe048bccfdc6aa9f4a991d
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-09-28 20:52:40 -0300

    feat: create my account details in home dashboard

---
+++ frontend/src/view/components/icons/BankAccountTypeIcon/iconsMap.ts
@@ -0,0 +1,9 @@
+import { CashIcon } from "./CashIcon";
+import { CheckingIcon } from "./CheckingIcon";
+import { InvestmentIcon } from "./InvestimentIcon";
+
+export const iconsMap = {
+  CHECKING: CheckingIcon,
+  INVESTMENT: InvestmentIcon,
+  CASH: CashIcon,
+}
